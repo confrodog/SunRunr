db8ec7fdbff13c2ff3d392b25c5a39635aa3ab84
Connor Musick <cjmusick48@gmail.com>
2019-12-02 17:58:05 -0700

    testing

--- routes/devices.js
+++ routes/devices.js
@@ -105,21 +105,20 @@ router.post('/register', function(req, res, next) {
                     // This following is equivalent to: res.status(400).send(JSON.stringify(responseJson));
                     return res.status(400).json(responseJson);
                 } else {
-                    User.findOne({email: email},(err,user)=>{
+                    User.findOneAndUpdate({email: email},{$push:{userDevices: req.body.deviceId}},(err,user)=>{
                         console.log("user: "+ user);
-                        if(user !== null){
-                            user.userDevices.push(newDevice.deviceId);
-                            user.save(done);
+                        if(err){
+                            responseJson.message = err;
+                            // This following is equivalent to: res.status(400).send(JSON.stringify(responseJson));
+                            return res.status(400).json(responseJson);
+                        }
+                        else{
                             responseJson.registered = true;
                             responseJson.apikey = deviceApikey;
                             responseJson.deviceId = req.body.deviceId;
                             responseJson.message = "Device ID " + req.body.deviceId + " was registered and added to "+user.email+" list.";
                             return res.status(201).json(responseJson);
-                        }
-                        else{
-                            responseJson.message = err;
-                            // This following is equivalent to: res.status(400).send(JSON.stringify(responseJson));
-                            return res.status(400).json(responseJson);
+                            
                         }
                         
                     });
