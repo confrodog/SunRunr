2b5dba30ff34876b9ac3a4f99767d702923e3187
Connor Musick <cjmusick48@gmail.com>
2019-12-02 19:39:25 -0700

    moved DELETE to POST

--- public/javascripts/account.js
+++ public/javascripts/account.js
@@ -125,7 +125,7 @@ function removeDevice(e){
   console.log(e.data.deviceId);
   $.ajax({
     url: '/devices/remove',
-    type: 'POST',
+    type: 'GET',
     headers: { 'x-auth': window.localStorage.getItem("authToken") },  
     contentType: 'application/json',
     data: { deviceId: e.data.deviceId }, 
